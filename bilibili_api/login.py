4e3262b6bee9bb2c5580abd3a34030270e25cb93
bqsgwys <me@wys.email>
2021-02-25 17:30:55 +0800

    fix indents

--- bilibili_api/login.py
+++ bilibili_api/login.py
@@ -2,12 +2,12 @@ r"""
 模块：login
 功能：登录功能
 项目GitHub地址：https://github.com/bqsgwys/bilibili_api
-    _____         ____         _____        ____     
- |    __ \     / __ \     / ____|    / ___\    
- | |__) | | |    | | | (___     | | ___
- |    __    \ | |    | |    \___ \    | | |_ |    
- | |__) | | |_\\ |    ____) | | |__| | 
- |    ___/     \___\\    |_____/     \____/            
+  _____     ____      _____    ____     
+ |  __ \   / __ \   / ____|   / ___\    
+ | |__) | | |  | | ( |___    | |      
+ |  __ \  | |  | |  \____ \  | | |_ |    
+ | |__) | | |_\\ |    ___) | | |__| | 
+ |_____/   \___\\   |_____/   \____/            
 """
 
 from . import exceptions, utils
@@ -20,45 +20,45 @@ import time
 API = utils.get_api()
 
 def print_login_qr_code() -> str:
-        """
-        获取登录二维码
-        :return oauthKey:返回的密钥 
-        """
-        api = API["login"]["qrcode"]["get_qr_code"]
-        ret = utils.get(url=api["url"])
-        qrcode_terminal.draw(ret["url"])
-        return ret['oauthKey']
+    """
+    获取登录二维码
+    :return oauthKey:返回的密钥 
+    """
+    api = API["login"]["qrcode"]["get_qr_code"]
+    ret = utils.get(url=api["url"])
+    qrcode_terminal.draw(ret["url"])
+    return ret['oauthKey']
 
 def get_qr_code_login_status(oauthKey:str) -> dict    : 
-        """
-        获取登录信息
-        :param oauthKey:登录密钥 
-        :return dict:{
-            status: 0=success else exception
-            verify:Verify格式的cookie
+    """
+    获取登录信息
+    :param oauthKey:登录密钥 
+    :return dict:{
+        status: 0=success else exception
+        verify:Verify格式的cookie
+    }
+    """
+    
+    if not (oauthKey):
+        raise exceptions.NoIdException
+    api = API["login"]["qrcode"]["get_login_info"]
+    data = {
+        "oauthKey": oauthKey
+    }
+    try:
+        ret = utils.post(url=api["url"],data=data,cookies={})
+        cookies = dict(urlparse.parse_qsl(urlparse.urlparse(ret['url']).query))
+        return {
+            'status': 1,
+            'verify': utils.Verify(sessdata = cookies['SESSDATA'], csrf = cookies['bili_jct'])
         }
-        """
-        
-        if not (oauthKey):
-                raise exceptions.NoIdException
-        api = API["login"]["qrcode"]["get_login_info"]
-        data = {
-                "oauthKey": oauthKey
+    except exceptions.BilibiliException as e:
+        return {
+            'status': e.code,
+            'verify': utils.Verify()
         }
-        try:
-            ret = utils.post(url=api["url"],data=data,cookies={})
-            cookies = dict(urlparse.parse_qsl(urlparse.urlparse(ret['url']).query))
-            return {
-                'status': 1,
-                'verify': utils.Verify(sessdata = cookies['SESSDATA'], csrf = cookies['bili_jct'])
-            }
-        except exceptions.BilibiliException as e:
-            return {
-                'status': e.code,
-                'verify': utils.Verify()
-            }
-        except Exception as e:
-            raise e
+    except Exception as e:
+        raise e
 
 def qrcode_login(timeout:float = 120) -> utils.Verify:
     """
